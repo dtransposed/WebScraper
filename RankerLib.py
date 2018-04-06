d510d3931da70a2b70a37abb8a91c0d0a19a2897
d.transposed <damian.bogunowicz@gmail.com>
2018-04-06 20:55:53 +0200

    Update RankerLib.py

--- RankerLib.py
+++ RankerLib.py
@@ -12,7 +12,9 @@ import matplotlib.pyplot as plt
 
 class Ranker_NN():
         '''
-        Ranker takes three arguments: n_classes (=2), number of fully connected nodes for custom layer 1 and 2   
+        Ranker takes three arguments: n_classes (=1), number of fully connected nodes for custom layer 1 and 2  
+        CNN is composed out of freezed ResNet layers and additional custom-made
+        fully connected layers. It returns an object 'model_final', which is our CNN model.
         '''
         def __init__(self,n_classes,n_fully_connected_1,n_fully_connected_2):
             self.n_classes=n_classes     
@@ -21,26 +23,24 @@ class Ranker_NN():
             self.resnet=applications.resnet50.ResNet50(include_top=False, weights='imagenet',input_shape=(224,224,3))
             for layer in self.resnet.layers:
                 layer.trainable = False
-        '''
-        convolutional_neural_network method creates our custom CNN. 
-        CNN is composed out of freezed ResNet layers and additional custom-made
-        fully connected layers. It returns an object 'model_final', which is our CNN model.
-        '''
-        def convolutional_neural_network(self):
             x=self.resnet.output
             x=Flatten()(x)
             x=Dense(self.fc1, activation="relu")(x)
             x=Dropout(0.5)(x)
             x=Dense(self.fc2, activation="relu")(x)
-            output=Dense(self.n_classes,activation='softmax')(x)
+            output=Dense(self.n_classes,activation='sigmoid')(x)
             self.model_final=Model(input=self.resnet.input,output=output)
-            return self.model_final
             '''
             make_prediction method allows us to perform pass through the CNN.
             It takes images from the string 'raw_image_source', which is the path and
             returns a returns a numpy array of size (no_of_images,2),
             where two elements in every row correspond to two results of sigmoid operation
             (sum up to 1) for each training sample. .
+            '''
+            '''
+            def make_prediction(self,np_array_from_Piotr):
+                return self.model_final.prediction(np_array_from_Piotr)
+            
             '''
         def make_prediction(self,raw_image_source):
             list_of_raw_files=os.listdir(raw_image_source)
@@ -58,7 +58,7 @@ class Ranker_NN():
             return prediction_list
 
         def train_model(self, data, true_labels, batch_size, no_epochs):
-            self.model_final.compile(optimizer = 'adam', loss = 'categorical_crossentropy',metrics=['accuracy'])
+            self.model_final.compile(optimizer = 'adam', loss = 'binary_crossentropy',metrics=['accuracy'])
             self.history=self.model_final.fit(data, true_labels, epochs = no_epochs, batch_size = batch_size)
             
         def evaluation_plot(self):
@@ -73,15 +73,10 @@ class Ranker_NN():
             self.model_final.save('GandeeCNN.h5')
 
 def create_dummy_labels(no_of_elements):
-    dummy_list=np.array([[0,0]])
+    dummy_list=np.array([[0]])
     possible_labels=[0,1]
     for number in range(no_of_elements):
-        a=random.choice(possible_labels)
-        if a==1:
-            b=0
-        else:
-            b=1
-        row=np.array([[a,b]])
+        row=np.array([[random.choice(possible_labels)]])
         print(row)
         dummy_list=np.concatenate((dummy_list, row), axis=0)
     dummy_list = np.delete(dummy_list, (0), axis=0)
@@ -97,4 +92,3 @@ def create_training_data(raw_image_source):
         x_image=applications.resnet50.preprocess_input(x)
         dummy_list=np.vstack((dummy_list, x_image))
     dummy_list = np.delete(dummy_list, (0), axis=0)
-    return dummy_list
